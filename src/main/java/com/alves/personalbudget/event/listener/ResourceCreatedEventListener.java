fd4cf19cb339a4076a3a9b42b3aef29db131232b
andrericardoalves <andrecajuru@gmail.com>
2021-12-25 23:09:42 +0000

    Add EventListener and Readme.md

---
+++ src/main/java/com/alves/personalbudget/event/listener/ResourceCreatedEventListener.java
@@ -0,0 +1,25 @@
+package com.alves.personalbudget.event.listener;
+
+import com.alves.personalbudget.event.ResourceCreatedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+
+@Component
+public class ResourceCreatedEventListener implements ApplicationListener<ResourceCreatedEvent> {
+    @Override
+    public void onApplicationEvent(ResourceCreatedEvent resourceCreatedEvent) {
+        HttpServletResponse response = resourceCreatedEvent.getResponse();
+        Long id = resourceCreatedEvent.getId();
+        addHeaderLocation(response, id);
+    }
+
+    private void addHeaderLocation(HttpServletResponse response, Long id) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(id).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+    }
+}
